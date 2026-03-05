530f30c0d54946491712ebfd41a7bf76c5646b45
Daniel Mesa <dmesa1994@gmail.com>
2026-03-04 21:11:11 -0500

    Update Wiredoor formula description and SHA256 checksum for ARM architecture

--- Formula/wiredoor.rb
+++ Formula/wiredoor.rb
@@ -1,13 +1,13 @@
 class Wiredoor < Formula
-  desc "Wiredoor CLI"
+  desc "Command-line client for managing Wiredoor"
   homepage "https://github.com/wiredoor/wiredoor-cli"
   version "1.2.0"
   license "Apache-2.0"
-  
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/wiredoor/wiredoor-cli/releases/download/v1.2.0/wiredoor_1.2.0-1_darwin_arm64.tar.gz"
-      sha256 "d0e29ebf1fafc2e720a34cc0137c4112f33be5a4bf6c0c05a27a25b2093737b1"
+      sha256 "8563d375a0acfe9b6a4bdaaded0b5e5de08c577d58233ddd3a61bf6176225ddf"
     else
       url "https://github.com/wiredoor/wiredoor-cli/releases/download/v1.2.0/wiredoor_1.2.0-1_darwin_amd64.tar.gz"
       sha256 "d0e29ebf1fafc2e720a34cc0137c4112f33be5a4bf6c0c05a27a25b2093737b1"
